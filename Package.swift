a1dc8ecee09c99ca724067c61cf3a3295d1d9667
김태현 <taehyk528@gmail.com>
2022-04-29 18:40:13 +0900

    [Edit] iOS 플랫폼 버전 명시(v13), 라이브러리 이름 변경(MDS)

--- Package.swift
+++ Package.swift
@@ -5,10 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "MDS-iOS",
+    
+    platforms: [
+        .iOS(.v13)
+    ],
+    
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MDS-iOS",
+            name: "MDS",
             targets: ["MDS-iOS"]),
     ],
     dependencies: [
