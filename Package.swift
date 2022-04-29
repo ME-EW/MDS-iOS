84c51a7a988fdd41d1322bdc472825949f43260d
김태현 <taehyk528@gmail.com>
2022-04-29 18:07:36 +0900

    [Edit] test target 제거

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "MDS-iOS",
             dependencies: []),
-        .testTarget(
-            name: "MDS-iOSTests",
-            dependencies: ["MDS-iOS"]),
     ]
 )
