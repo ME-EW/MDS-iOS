e90a10f2783fd712bb169837968130ca0d0dbc82
김태현 <taehyk528@gmail.com>
2022-04-29 18:55:51 +0900

    [Add] MDSBaseColor UIColor Bundle 추가

--- Sources/Foundation/MDSBaseColor.swift
+++ Sources/Foundation/MDSBaseColor.swift
@@ -17,21 +17,21 @@ public enum MDSBaseColor {
     
     // MARK: - Character
     
-    public static let purpleNew = UIColor(named: "purple_new")
-    public static let redNew = UIColor(named: "red_new")
-    public static let blueNew = UIColor(named: "blue_new")
-    public static let pinkNew = UIColor(named: "pink_new")
-    public static let orangeNew = UIColor(named: "orangeNew")
-    public static let yellowNew = UIColor(named: "yellow_new")
+    public static let purpleNew = UIColor(named: "purple_new", in: .module, compatibleWith: nil)
+    public static let redNew = UIColor(named: "red_new", in: .module, compatibleWith: nil)
+    public static let blueNew = UIColor(named: "blue_new", in: .module, compatibleWith: nil)
+    public static let pinkNew = UIColor(named: "pink_new", in: .module, compatibleWith: nil)
+    public static let orangeNew = UIColor(named: "orangeNew", in: .module, compatibleWith: nil)
+    public static let yellowNew = UIColor(named: "yellow_new", in: .module, compatibleWith: nil)
     
     // MARK: - Grey Scale
     
-    public static let black = UIColor(named: "black")
-    public static let grey700 = UIColor(named: "grey700")
-    public static let grey600 = UIColor(named: "grey600")
-    public static let grey500 = UIColor(named: "grey500")
-    public static let grey400 = UIColor(named: "grey400")
-    public static let grey300 = UIColor(named: "grey300")
-    public static let grey200 = UIColor(named: "grey200")
-    public static let white = UIColor(named: "white")
+    public static let black = UIColor(named: "black", in: .module, compatibleWith: nil)
+    public static let grey700 = UIColor(named: "grey700", in: .module, compatibleWith: nil)
+    public static let grey600 = UIColor(named: "grey600", in: .module, compatibleWith: nil)
+    public static let grey500 = UIColor(named: "grey500", in: .module, compatibleWith: nil)
+    public static let grey400 = UIColor(named: "grey400", in: .module, compatibleWith: nil)
+    public static let grey300 = UIColor(named: "grey300", in: .module, compatibleWith: nil)
+    public static let grey200 = UIColor(named: "grey200", in: .module, compatibleWith: nil)
+    public static let white = UIColor(named: "white", in: .module, compatibleWith: nil)
 }
