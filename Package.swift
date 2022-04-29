aed13602a2a33fc38999c0c3dabd4c285dafb65f
김태현 <taehyk528@gmail.com>
2022-04-29 20:20:42 +0900

    [Fix] MDSIcon Path process에서 copy로 변경

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             path: "Sources",
             resources: [
                 .process("Resource/Color.xcassets"),
-                .process("Resource/MDSIcon.xcassets"),
+                .copy("Resource/MDSIcon.xcassets"),
             ]
         ),
     ],
