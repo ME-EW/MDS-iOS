0373000b91a719472b4ed0245e7438a303494cb1
김태현 <taehyk528@gmail.com>
2022-04-29 18:34:46 +0900

    [Add] Color Resource

---
+++ Sources/MDS-iOS/Foundation/MDSBaseColor.swift
@@ -0,0 +1,37 @@
+//
+//  MDSBaseColor.swift
+//  
+//
+//  Created by taehy.k on 2022/04/29.
+//
+
+import UIKit
+
+/**
+ - Description
+ 
+    모든 MDS Color에 적용되는 컬러입니다.
+ */
+
+public enum MDSBaseColor {
+    
+    // MARK: - Character
+    
+    public static let purpleNew = UIColor(named: "purple_new")
+    public static let redNew = UIColor(named: "red_new")
+    public static let blueNew = UIColor(named: "blue_new")
+    public static let pinkNew = UIColor(named: "pink_new")
+    public static let orangeNew = UIColor(named: "orangeNew")
+    public static let yellowNew = UIColor(named: "yellow_new")
+    
+    // MARK: - Grey Scale
+    
+    public static let black = UIColor(named: "black")
+    public static let grey700 = UIColor(named: "grey700")
+    public static let grey600 = UIColor(named: "grey600")
+    public static let grey500 = UIColor(named: "grey500")
+    public static let grey400 = UIColor(named: "grey400")
+    public static let grey300 = UIColor(named: "grey300")
+    public static let grey200 = UIColor(named: "grey200")
+    public static let white = UIColor(named: "white")
+}
--- Sources/MDS-iOS/MDS_iOS.swift
+++
@@ -1,6 +0,0 @@
-public struct MDS_iOS {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
